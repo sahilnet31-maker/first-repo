b8e2b91d581f7ecd56146bc229839029766dcb20
sahil <sahilnet31@gmail.com>
2025-09-14 19:56:49 +0530

    Add DestinationsController for TravelApi

---
+++ TravelApi/Controllers/DestinationsController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DestinationsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var destinations = new[]
+            {
+                new { Id = 1, Name = "Paris" },
+                new { Id = 2, Name = "New York" },
+                new { Id = 3, Name = "Tokyo" }
+            };
+            return Ok(destinations);
+        }
+    }
+}
